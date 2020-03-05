37212a25c0c412741cadf0fc962e0203838d13cb
tchambers14 <tchambers14@googlemail.com>
2020-03-05 23:53:01 +0000

    Added message flashing

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template, url_for
+from flask import render_template, url_for, flash, redirect
 from app.forms import LoginForm
 
 
@@ -9,7 +9,10 @@ def index():
     return render_template('base.html')
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET','POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+        return redirect('/index')
     return render_template('login.html', title='Sign In', form=form)
