caea8855eda9c897d09dafebee5cb16faa54fe73
tchambers14 <tchambers14@googlemail.com>
2020-03-05 23:37:41 +0000

    Added Loginform

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,15 @@
 from app import app
 from flask import render_template, url_for
+from app.forms import LoginForm
 
 
 @app.route('/', methods=['GET'])
-def hello_world():
+@app.route('/index', methods=['GET'])
+def index():
     return render_template('base.html')
 
-@app.route('/<name>')
-def hello_name(name):
-    return "Hello {}!".format(name)
 
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
--- config.py
+++ config.py
@@ -7,9 +7,9 @@ class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    SECRET_KEY = os.environ['SECRET_KEY'] or 'TEST'  # TODO validate that this approach will work
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'TEST'  # TODO validate that this approach will work
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL'] or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
 
 class ProductionConfig(Config):
