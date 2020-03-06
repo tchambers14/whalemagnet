6fc165f18aba3e41a069fb58b5e8d934e1f0789b
tchambers14 <tchambers14@googlemail.com>
2020-03-06 00:18:48 +0000

    Updates to the login and logout features

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,12 @@ import os
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+login = LoginManager(app)
 
 from app import routes, models
